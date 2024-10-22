d99070bdd77735b84b6374dd3f399e631377ad2f
DalineSeritsane <dalinelerato@gmail.com>
2024-10-22 16:15:44 +0200

    singPost updates

--- src/pages/single/SinglePost.jsx
+++ src/pages/single/SinglePost.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./singlePost.css";
-import BodyWater from "../../Image/BodyWater.jpg";
+import paris from "../../Image/paris.jpg";
 
 function SinglePost() {
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
-        <img className="singlePostImg" src={BodyWater} alt='' />
+        <img className="singlePostImg" src={paris} alt='' />
         <h1 className="singlePostTitle">Lorem ipsum dolor si.
             <div className="singlePostEdit">
              <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
@@ -15,9 +15,16 @@ function SinglePost() {
         </h1>
         <div className="singlePostInfo">
             <span className="singlePostAuthor">Author: <b>Swarts</b></span>
-            <span className="singlePostDate">1 hour ago</span>
+            <span className="singlePostDate"> 1 hour ago</span>
         </div>
-
+        <p className="singlePostDesc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia debitis voluptate vitae minus voluptatem magni, harum deserunt atque itaque. Ea officia nesciunt delectus cum! Ipsum, ullam. Tempora, dignissimos a.
+        em ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia debitis voluptate vitae minus voluptatem magni, harum deserunt atque itaque. Ea officia nesciunt delectus cum! Ipsum, ullam. Tempora, dignissimos a.
+        em ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia debitis voluptate vitae minus voluptatem magni, harum deserunt atque itaque. Ea officia nesciunt delectus cum! Ipsum, ullam. Tempora, dignissimos a.
+        em ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia debitis voluptate vitae minus voluptatem magni, harum deserunt atque itaque. Ea officia nesciunt delectus cum! Ipsum, ullam. Tempora, dignissimos a.
+        em ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia debitis voluptate vitae minus voluptatem magni, harum deserunt atque itaque. Ea officia nesciunt delectus cum! Ipsum, ullam. Tempora, dignissimos a.
+        em ipsum dolor sit amet consectetur adipisicing elit. Fuga mollitia debitis voluptate vitae minus voluptatem magni, harum deserunt atque itaque. Ea officia nesciunt delectus cum! Ipsum, ullam. Tempora, dignissimos a.
+        </p>
       </div>
     </div>
   )
