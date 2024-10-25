cb44efd4fdf835b6075c4f72081efab76c474dac
DalineSeritsane <dalinelerato@gmail.com>
2024-10-25 18:03:36 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-//import Home from "./pages/home/Home";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 import Navbar from "./Navbar/Navbar";
-//import Single from "./pages/single/Single";
-//import Write from "./pages/write/Write";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Setting";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
-    <>
-    <Navbar />
-    {/*<Home />*/}
-    {/*<Single />*/}
-    {/*<Write />*/}
-    <Setting />
-
-    
-
-
-    </>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/post/postId">
+          <Single />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
