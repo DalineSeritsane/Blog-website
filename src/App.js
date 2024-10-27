a1cfce1fefae6f19746e0e362ae6978e38fee3db
DalineSeritsane <dalinelerato@gmail.com>
2024-10-27 17:51:01 +0200

    about and contact updates

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Setting";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   const user = true;
@@ -17,6 +19,8 @@ function App() {
       <Route path="/" exact element={<Home></Home>}></Route> 
           <Route path="/register" element={user ? <Home/> : <Register />} ></Route>
           <Route path="/login" element={user ? <Home/> : <Login />} ></Route>
+          <Route path="/about" element={ <About />} ></Route>
+          <Route path="/contact" element={ <Contact />} ></Route>
           <Route path="/write" element={user ? <Write /> : <Register />} ></Route>
           <Route path="/setting" element={user ? <Setting /> : <Register/>} ></Route>
           <Route path="/post/:postId" element={<Single />} ></Route> 
