b1c9845029b2c509af1852802a2b10db4e1349f7
DalineSeritsane <dalinelerato@gmail.com>
2024-11-09 18:34:53 +0200

    error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./Navbar/Navbar";
 import Single from "./pages/single/Single";
